28783d9a286afb92796934ae8ecf5d98b4a1736b
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2024-06-06 17:20:21 +0530

    addeding import react

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./ExpenseDate.css"
 const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
 
