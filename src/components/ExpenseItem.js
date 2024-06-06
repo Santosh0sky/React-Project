c49f04dc62d188f69fc485da53154d39ba667d73
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2024-06-06 12:47:04 +0530

    css added

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,16 @@
-function ExpenseItem(){
-return(
-    <div><p>Food - $5</p>
-    <p>Petrol - $20</p>
-    <p>Movie - $10</p></div>
-);
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div>15 August 2023</div>
+      <did className = "expense-item__location">Delhi</did>
+      <div className = "expense-item__description">
+        <h2>Book</h2>
+        <div className = "expense-item__price">$10</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
