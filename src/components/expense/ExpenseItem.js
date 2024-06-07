58112a2b01af426b0a6725e33b6f7995e8079a21
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2024-06-07 15:11:24 +0530

    useState hook impelement

--- src/components/expense/ExpenseItem.js
+++ src/components/expense/ExpenseItem.js
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card"
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
   function buttonClickHandler(event){
-    console.log("button Clicked");
+    setTitle("New Title");
   }
 
   return(
@@ -13,7 +15,7 @@ const ExpenseItem = (props) => {
       <ExpenseDate date = {props.date} />
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
       <button onClick = {buttonClickHandler}>Change Title</button>
