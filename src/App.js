900d577148d23a263f0d43521f86d68af7027585
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2024-06-09 14:29:09 +0530

    addinf filter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, {useState} from "react";
 import Expenses from "./components/expense/Expenses"
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummyExpenses = [
+  { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
+  { id: "2", date: new Date(2024, 3, 25), title: "Book", price: 10 },
+  { id: "3", date: new Date(2025, 10, 11), title: "Pen", price: 1 },
+  { id: "4", date: new Date(2024, 1, 14), title: "Laptop", price: 200 },
+];
+
 const App = () => {
-  const [expenses, setExpense] = useState([
-    { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
-    { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
-    { id: "3", date: new Date(2023, 10, 11), title: "Pen", price: 1 },
-    { id: "4", date: new Date(2023, 1, 14), title: "Laptop", price: 200 },
-  ]);
+  const [expenses, setExpense] = useState(dummyExpenses);
 
   const expenseDataHandler = (newExpenseData) =>{
     
--- src/components/expense/Expenses.js
+++ src/components/expense/Expenses.js
@@ -1,20 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-    console.log(props.expense);
+        const [filteredYear, setFilteredYear] = useState("2023");
+      
+        const changeFilterHandler = (selectedYear) => {
+          setFilteredYear(selectedYear);
+        };
+
+
 return(
     <Card className = "expenses">
-        
+       <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={changeFilterHandler}/>
         { (props.expense).map((expense)=>{
+                 if(expense.date.getFullYear()==filteredYear){
                 return(
                 <ExpenseItem key = {expense.id} 
                 date = {expense.date}
                 title = {expense.title}
                 price = {expense.price}
-                />);
+                />)}
+                else{
+                    return ("");
+                }
             })
         }
     </Card>
