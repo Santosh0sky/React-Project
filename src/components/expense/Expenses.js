d2744fed4e7cc23f3d87cac8ad78d099aa329151
Santosh0sky <santosh.kr.yd.sky@gmail.com>
2024-06-09 18:16:19 +0530

    ADDED LIST FILTER

--- src/components/expense/Expenses.js
+++ src/components/expense/Expenses.js
@@ -15,20 +15,30 @@ const Expenses = (props) => {
                 return expense.date.getFullYear().toString() === filteredYear;
         });
 
+        let expensesContent = <p>No expenses found</p>;
+
+        if (filteredExpenses.length > 0) {
+          expensesContent = filteredExpenses.map((expense) => {
+            return (
+                <>
+              <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                date={expense.date}
+                price={expense.price}
+              />
+              {filteredExpenses.length === 1 && <p>Only one expense found. Please add more</p>}
+              </>
+            );
+          });
+        }
+
 return(
     <Card className = "expenses">
        <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}/>
-        { (filteredExpenses).map((expense)=>{
-                return(
-                <ExpenseItem key = {expense.id} 
-                date = {expense.date}
-                title = {expense.title}
-                price = {expense.price}
-                />);
-            })
-        }
+        {expensesContent}
     </Card>
 )
 }
